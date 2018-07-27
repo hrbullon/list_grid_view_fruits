e735525b8f78045944a4c659e65bc7ad4bba233b
Haderson Bullon <hrbullon@gmail.com>
2018-07-27 15:23:02 -0400

    Initialized project

---
+++ app/src/main/java/view/grid/list/fruits/examples/com/fruits/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package view.grid.list.fruits.examples.com.fruits.activities;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import view.grid.list.fruits.examples.com.fruits.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
